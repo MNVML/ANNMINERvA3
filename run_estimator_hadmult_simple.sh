5c970d39282cf02fade5228494b18eca051ae61f
Gabriel Perdue <gnperdue@me.com>
2018-09-07 12:46:01 -0500

    rework args for runner scripts

--- run_estimator_hadmult_simple.sh
+++ run_estimator_hadmult_simple.sh
@@ -3,12 +3,20 @@ EXE="estimator_hadmult_simple.py"
 BATCH_SIZE=100
 TRAIN_STEPS=5
 NUM_EPOCHS=1
-MODELDIR="/tmp/simple_hadmult_estimator"
 
+MODEL_DIR="/tmp/simple_hadmult_estimator"
 DATA_DIR="/Users/perdue/Dropbox/Data/RandomData/hdf5"
-TFRECORD=""
+TRAIN_FILE="${DATA_DIR}/hadmultkineimgs_mnvvtx_train.hdf5"
+EVAL_FILE="${DATA_DIR}/hadmultkineimgs_mnvvtx_test.hdf5"
 
-ARGS="--batch-size ${BATCH_SIZE} --train-steps ${TRAIN_STEPS} --num-epochs
-${NUM_EPOCHS} --data-dir ${DATA_DIR} $TFRECORD --model-dir $MODELDIR"
+ARGS="--batch-size ${BATCH_SIZE}"
+ARGS+=" --train-steps ${TRAIN_STEPS}"
+ARGS+=" --num-epochs ${NUM_EPOCHS}"
+ARGS+=" --train-file ${TRAIN_FILE}"
+ARGS+=" --eval-file ${EVAL_FILE}"
+ARGS+=" --model-dir ${MODEL_DIR}"
 
+cat << EOF
+python $EXE $ARGS
+EOF
 python $EXE $ARGS
